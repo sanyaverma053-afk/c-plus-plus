d6aab0c310e3c59db947e5f082a33437e6029820
SANYA VERMA <sanyaverma053@gmail.com>
2026-07-05 19:57:20 +0530

    Add main function with increment and decrement examples
    Implement basic increment and decrement operations on an integer.

---
+++ q8.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x=5;
+    x++;
+    cout<<x<<endl;
+    x--;
+    cout<<x<<endl;
+    ++x;
+    cout<<x<<endl;
+    --x;
+    cout<<x<<endl;
+}
