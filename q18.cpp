a55ad958d9a1983d26fa45dec7470171fe74cdcc
SANYA VERMA <sanyaverma053@gmail.com>
2026-07-07 20:50:20 +0530

    Add program to check divisibility by 3 and 5

---
+++ q18.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter n:";
+    cin>>n;
+    if(n%3==0 and n%5==0){
+        cout<<"divisile by 3 and 5";
+    }
+    else{
+        cout<<"not divisile by 3 and 5";
+    }
+}
