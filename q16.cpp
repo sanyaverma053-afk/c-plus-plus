b8f8d0c1e7d0cf52db5cec05906f3c82e669fd90
SANYA VERMA <sanyaverma053@gmail.com>
2026-07-07 20:48:21 +0530

    Add program to output absolute value of n

---
+++ q16.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter n:";
+    cin>>n;
+    if(n>=0){
+        cout<<n;
+    }
+    else{
+        cout<<-n;
+    }
+}
