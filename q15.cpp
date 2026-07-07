9a6a153975d6b5acf292d0b8f26ed82b8af73963
SANYA VERMA <sanyaverma053@gmail.com>
2026-07-07 20:47:43 +0530

    Add profit and loss calculation program

---
+++ q15.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int cp;
+    cout<<"enter cost price:";
+    cin>>cp;
+    int sp;
+    cout<<"enter selling price:";
+    cin>>sp;
+    if(sp>cp){
+        cout<<"profit is"<<sp-cp;
+    }
+    else if(cp>sp){
+        cout<<"loss is"<<cp-sp;
+    }
+    else{
+        cout<<"no profit no loss";
+    }
+}
