c3a07ec287e36f30c4b0fef9cb3f28403003c693
SANYA VERMA <sanyaverma053@gmail.com>
2026-07-15 22:08:36 +0530

    Add prime number check in q39.cpp

---
+++ q39.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter n:";
+    cin>>n;
+    bool flag=true;
+    for(int i=2;i<=n/2;i++){
+        if(n%i==0){
+            flag=false;
+            break;
+        }
+    }
+    if(flag==true)cout<<"Prime";
+    else cout<<"Composite";
+}
+    
