d159b81724fc9d4e5c928d4014278b46c20f15c0
SANYA VERMA <sanyaverma053@gmail.com>
2026-07-20 23:20:52 +0530

    Add a simple do-while loop to print numbers

---
+++ q44.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int num = 1;
+    do {
+        std::cout << "Number: " << num << "\n";
+        num++;
+    } while (num <= 3);  
+    return 0;
+}
